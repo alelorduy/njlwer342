6598233b54b40913c1389280af9803d5589d2247
alelorduy <alejandra_lorduy@hotmail.com>
2021-05-20 20:41:18 -0400

    Create new function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 
 /// Modifica el componente para que se puedan agregar tareas
 
-class App extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="list">
-          <h3>Por hacer:</h3>
-          <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
-          </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
-        </div>
+function Tasks() {
+  const [taskList, setTaskList] = useState([
+    "Sacar la ropa",
+    "Hacer la cama",
+    "Leer un rato",
+  ]);
+  const [newTask, setNewTask] = useState("");
+
+  const sendNewTask = (event) => {
+    event.preventDefault();
+    setTaskList([...taskList, newTask]);
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="list">
+        <h3>Por hacer:</h3>
+        <ul className="todo">
+          {taskList.map((task) => (
+            <li>{task}</li>
+          ))}
+        </ul>
+        <form onSubmit={(event) => sendNewTask(event)}>
+          <input
+            value={newTask}
+            type="text"
+            id="new-task"
+            placeholder="Ingresa una tarea y oprime Enter"
+            onChange={(event) => setNewTask(event.target.value)}
+          />
+        </form>
       </div>
-    )
-  }
+    </div>
+  );
 }
-
-
-export default App;
+export default Tasks;
